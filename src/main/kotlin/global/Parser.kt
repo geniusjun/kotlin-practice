250ac23d1dfadff70487c1eb74b5c62514dca732
geniusjun <geniusjun4663@gmail.com>
2025-11-07 19:09:13 +0900

    Feat(Parser): 문자열을 Number로 바꿔주는 파싱 로직 구현

--- src/main/kotlin/global/Parser.kt
+++ src/main/kotlin/global/Parser.kt
@@ -1,5 +1,6 @@
 package org.example.global
 
+import org.example.domain.LottoNumber
 import org.example.global.constants.ErrorMessage
 
 object Parser {
@@ -7,6 +8,14 @@ object Parser {
     fun stringToInt(input: String): Int {
         return input.toIntOrNull()
             ?: throw IllegalArgumentException(ErrorMessage.NUMBER_FORMAT_ERROR.message)
+    }
+    
+    fun stringToNumbers(text: String): List<LottoNumber> {
+        return text
+            .split(",")
+            .map { it.trim() }
+            .map { stringToInt(it) }
+            .map { LottoNumber.valueOf(it) }
     }
 
 }
