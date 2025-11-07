8e542eb60e10638273456e01c26854ee93e0d76c
geniusjun <geniusjun4663@gmail.com>
2025-11-07 20:50:38 +0900

    Feat(Lottos): 로또들을 저장할 도메인 클래스 생성

---
+++ src/main/kotlin/domain/Lottos.kt
@@ -0,0 +1,18 @@
+package org.example.domain
+
+class Lottos private constructor(lottos: List<Lotto>) {
+
+    private val lottos: List<Lotto> = lottos.toList()
+
+    companion object {
+        fun from(lottos: List<Lotto>): Lottos {
+            return Lottos(lottos)
+        }
+    }
+
+    fun size(): Int = lottos.size
+
+    fun asList(): List<Lotto> = lottos
+
+    fun asSequence(): Sequence<Lotto> = lottos.asSequence()
+}
