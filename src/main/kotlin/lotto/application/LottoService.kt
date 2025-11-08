671c4189a335e3ff11cb2af7bb2fe3f10d736ab6
geniusjun <geniusjun4663@gmail.com>
2025-11-08 19:22:47 +0900

    Feat(LottoService): 각 도메인 생성 로직 구현
    직접적인 도메인 생성 로직은 Service 레이어에서 실행하게끔 하고 controller는 이를 호출하는 구조로 설계한다.

--- src/main/kotlin/lotto/application/LottoService.kt
+++ src/main/kotlin/lotto/application/LottoService.kt
@@ -1,9 +1,14 @@
 package lotto.application
 
-import lotto.domain.Cost
-import lotto.domain.Lottos
+import lotto.domain.*
+import lotto.global.Parser
 
-class LottoService(private val lottoFactory: LottoFactory) {
+class LottoService(
+    private val lottoFactory: LottoFactory
+) {
+    /**
+     * 검증된 Cost로부터 로또 여러 장 구매
+     */
     fun buyLottos(cost: Cost): Lottos {
         val lottos = buildList {
             repeat(cost.count) {
@@ -11,5 +16,25 @@ class LottoService(private val lottoFactory: LottoFactory) {
             }
         }
         return Lottos.from(lottos)
+    }
+
+    /**
+     * 사용자가 입력한 문자열을 비용 도메인으로 변환
+     */
+    fun parseCost(input: String): Cost =
+        Cost.from(input)
+
+    /**
+     * 당첨 번호(6개) 문자열을 Lotto로 변환
+     */
+    fun parseWinningLotto(input: String): Lotto =
+        Lotto.from(Parser.stringToNumbers(input))
+
+    /**
+     * 보너스 번호 문자열을 Bonus로 변환
+     */
+    fun parseBonus(input: String, winning: Lotto): Bonus {
+        val number = LottoNumber.valueOf(Parser.stringToInt(input))
+        return Bonus.of(number, winning)
     }
 }
